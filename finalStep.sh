ca54778ecbab99f5f67f9f82665565fd77ac5d38
christoulakis.foivos <christoulakis.foivos@R90NELLK.adlittle.com>
2017-10-12 15:49:36 +0100

    test

--- finalStep.sh
+++ finalStep.sh
@@ -3,6 +3,10 @@ SERVICE_NAME="adl-gallery-ws-service"
 
 TASK_FAMILY="deploysimpleserver"
 
+echo ${WORKSPACE}
+echo ${JENKINS_HOME}
+
+
 TASK_REVISION=`sudo aws ecs describe-task-definition --task-definition deploysimpleserver | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 
 #DESIRED_COUNT=`sudo aws ecs describe-services --cluster twoinstance1 --services adl-gallery-ws-service | egrep "desiredCount"  | tr "/" " "| awk '{print $2}'| sed 's/,$//'| sed -n 1p`
