2a2ce49e07c14517efcf7474d183c4ff2bb5e0d8
christoulakis.foivos <christoulakis.foivos@R90NELLK.adlittle.com>
2017-10-12 15:40:33 +0100

    workaround: add shell script

---
+++ finalStep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SERVICE_NAME="adl-gallery-ws-service"
+
+TASK_FAMILY="deploysimpleserver"
+
+TASK_REVISION=`sudo aws ecs describe-task-definition --task-definition deploysimpleserver | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+
+DESIRED_COUNT=`sudo aws ecs describe-services --cluster twoinstance1 --services adl-gallery-ws-service | egrep "desiredCount"  | tr "/" " "| awk '{print $2}'| sed 's/,$//'| sed -n 1p`
+
+echo $TASK_REVISION
+
+echo $DESIRED_COUNT
+
+sudo aws ecs update-service --cluster twoinstance1 --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
